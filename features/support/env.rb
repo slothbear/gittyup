f2a200e6afaeebe13b9a73fa4d863bd21d81aaaf
Paul Morganthall <paul@morganthall.com>
2013-02-17 20:48:11 -0500

    Clean up test files using more specific names.

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,7 +4,13 @@ Before do
   # The default ./tmp/aruba directory can't be used because it is a 
   # subdir of the gem directory, already part of a git repository.
   # We are responsible for cleaning the directory on startup.
-  @dirs = ['/tmp/aruba/adcopu']
-  FileUtils.rm_rf(@dirs, :secure => true)
+  # Being careful, we only clean items we know we created.
+  temp_dir = '/tmp/aruba/adcopu/'
+  FileUtils.rm_rf(temp_dir + 'repository/.git', :secure => true)
+  FileUtils.rm_f(temp_dir + 'repository/railsbridge-boston.rb')
+  FileUtils.rmdir(temp_dir + 'repository')
+  FileUtils.rmdir(temp_dir)
+
+  @dirs = [temp_dir]
 end
 
